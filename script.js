4bb7dfda2fc2a02c4f5cd2db660a0b48fe77f289
Junior1511 <yunior122000@gmail.com>
2024-06-27 19:58:10 -0500

    gotta work more on html and css, and then proceed with the logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// I guess I have to work on the logic first
+function add (a,b){
+    return a + b
+}
+function subtract (a,b){
+    return a - b
+}
+function multiply (a,b){
+    return a * b
+}
+function divide (a,b){
+    return a / b
+}
+
+// Variables to update display 
+let firstNum;
+let operator;
+let lastNum;

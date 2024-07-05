f2a4d4b6debd1d20404eb63c4ca0c7ae853dfeb9
Junior1511 <yunior122000@gmail.com>
2024-07-05 17:01:20 -0500

    working on the logic still

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ const divide = document.querySelector("#divide")
 const add = document.querySelector("#add")
 const subtract = document.querySelector("#subtract")
 const equals = document.querySelector("#equals")
-let firstNum;
-let operator;
-let lastNum;
+// let firstNum;
+// let operator;
+// let lastNum;
 let math = [];
 
 // Step 3
@@ -44,7 +44,43 @@ function operate (){
     let wtf = eval(dude)
     console.log(wtf)
 }
-equals.addEventListener("click", function (e){
-    display.textContent = `${equals.textContent}`
-    console.log("idk dude")
+num1.addEventListener("click", function (e){
+    display.textContent = `${num1.textContent}`
+    math.push(num1.textContent)
+})
+num2.addEventListener("click", function (e){
+    display.textContent = `${num2.textContent}`
+    math.push(num2.textContent)
+})
+num3.addEventListener("click", function (e){
+    display.textContent = `${num3.textContent}`
+    math.push(num3.textContent)
+})
+num4.addEventListener("click", function (e){
+    display.textContent = `${num4.textContent}`
+    math.push(num4.textContent)
+})
+num5.addEventListener("click", function (e){
+    display.textContent = `${num5.textContent}`
+    math.push(num5.textContent)
+})
+num6.addEventListener("click", function (e){
+    display.textContent = `${num6.textContent}`
+    math.push(num6.textContent)
+})
+num7.addEventListener("click", function (e){
+    display.textContent = `${num7.textContent}`
+    math.push(num7.textContent)
+})
+num8.addEventListener("click", function (e){
+    display.textContent = `${num8.textContent}`
+    math.push(num8.textContent)
+})
+num9.addEventListener("click", function (e){
+    display.textContent = `${num9.textContent}`
+    math.push(num9.textContent)
+})
+num0.addEventListener("click", function (e){
+    display.textContent = `${num0.textContent}`
+    math.push(num0.textContent)
 })

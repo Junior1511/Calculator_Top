4e2095b32d1052b625c96903be4887cb293afc9b
Junior1511 <yunior122000@gmail.com>
2024-07-16 22:18:44 -0500

    I'm not completely sure where im at, but I made it work even with decimals. I gotta make it work with negative numbers and see if there is any problems with it. and add a AC and DELETE button

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function add (a,b){
-    return a + b
+    return parseFloat(a) + parseFloat(b)
 }
 function subtract (a,b){
-    return a - b
+    return parseFloat(a) - parseFloat(b)
 }
 function multiply (a,b){
-    return a * b
+    return parseFloat(a) * parseFloat(b)
 }
 function divide (a,b){
-    return a / b
+    return parseFloat(a) / parseFloat(b)
 }
 
 // Variables
@@ -32,135 +32,144 @@ const division = document.querySelector("#divide")
 const addition = document.querySelector("#add")
 const subtraction = document.querySelector("#subtract")
 const equals = document.querySelector("#equals")
-let firstNum = [];
+let firstNum = "";
 let operator;
-let lastNum = [];
+let lastNum = "";
 // Step 3
 function operate (){
 if (operator == "+"){
-    display.textContent= add(firstNum.toString,lastNum.toString)
+    display.textContent = `${add(firstNum , lastNum)}`
+    firstNum = [display.textContent]
 } else if (operator == "-"){
-    display.textContent= subtract(firstNum.toString,lastNum.toString)
+    display.textContent = `${subtract(firstNum , lastNum)}`
+    firstNum = [display.textContent]
 } else if (operator == "x"){
-    display.textContent= multiply(firstNum.toString,lastNum.toString)
+    display.textContent = `${multiply(firstNum , lastNum)}`
+    firstNum = [display.textContent]
 } else if (operator == "/"){
-    display.textContent= divide(firstNum.toString,lastNum.toString)
+    display.textContent = `${divide(firstNum , lastNum)}`
+    firstNum = [display.textContent]
 }
 }
 // 
 // 
 num1.addEventListener("click", function (e){
 if (operator == undefined){
-    firstNum.push("1")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+    firstNum = firstNum.concat("1")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("1")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
+    lastNum = lastNum.concat("1")
+    display.textContent = `${lastNum}`
 }
 })
 num2.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("2")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("2")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("2")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
+    lastNum = lastNum.concat("2")
+    display.textContent = `${lastNum}`
 }
 })
 num3.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("3")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("3")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("3")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+    lastNum = lastNum.concat("3")
+    display.textContent = `${lastNum}`
+}    
 })
 num4.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("4")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("4")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("4")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+    lastNum = lastNum.concat("4")
+    display.textContent = `${lastNum}`
+}    
 })
 num5.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("5")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("5")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("5")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+    lastNum = lastNum.concat("5")
+    display.textContent = `${lastNum}`
+}    
 })
 num6.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("6")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("6")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("6")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+    lastNum = lastNum.concat("6")
+    display.textContent = `${lastNum}`
+}   
 })
 num7.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("7")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("7")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("7")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+    lastNum = lastNum.concat("7")
+    display.textContent = `${lastNum}`
+}    
 })
 num8.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("8")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("8")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("8")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+    lastNum = lastNum.concat("8")
+    display.textContent = `${lastNum}`
+}   
 })
 num9.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("9")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("9")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("9")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
+    lastNum = lastNum.concat("9")
+    display.textContent = `${lastNum}`
 }
 })
 num0.addEventListener("click", function (e){
-    if (operator == undefined){
-    firstNum.push("0")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
+if (operator == undefined){
+    firstNum = firstNum.concat("0")
+    display.textContent = `${firstNum}`
 } else {
-    lastNum.push("0")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
+    lastNum = lastNum.concat("0")
+    display.textContent = `${lastNum}`
 }
 })
 period.addEventListener("click", function (e){
     if (operator == undefined){
-    firstNum.push(".")
-    display.textContent = `${firstNum.join().replaceAll(",","")}`
-} else {
-    lastNum.push(".")
-    display.textContent = `${lastNum.join().replaceAll(",","")}`
-}
+        firstNum = firstNum.concat(".")
+        display.textContent = `${firstNum}`
+    } else {
+        lastNum = lastNum.concat(".")
+        display.textContent = `${lastNum}`
+    }
 })
-// negative.addEventListener("click", function (e){
-//     if (operator == undefined){
-//     firstNum.push("-")
-//     display.textContent = `${firstNum.join().replaceAll(",","")}`
-// } else {
-//     lastNum.push("-")
-//     display.textContent = `${lastNum.join().replaceAll(",","")}`
-// }
-// })
+negative.addEventListener("click", function (e){
+
+})
 addition.addEventListener("click", function (e){
     operator = "+"
+})
+subtraction.addEventListener("click", function (e){
+    operator = "-"
+})
+division.addEventListener("click", function (e){
+    operator = "/"
+})
+multiplication.addEventListener("click", function (e){
+    operator = "x"
 })
 
 equals.addEventListener("click", function (e){
     operate()
+    operator = undefined
+    lastNum = ""
 })
